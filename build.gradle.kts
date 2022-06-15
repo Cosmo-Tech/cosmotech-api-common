23b74e8874198dd11977398271c8760b4d9fa6ce
jreynard <jeremy.reynard@cosmotech.com>
2022-06-15 11:04:17 +0200

    Correct springOauthVersion set 5.7.1 + Remove all compileOnly

--- build.gradle.kts
+++ build.gradle.kts
@@ -113,11 +113,9 @@ val swaggerParserVersion = "2.0.31"
 val hashidsVersion = "1.0.3"
 val springOauthAutoConfigureVersion = "2.6.6"
 val springSecurityJwtVersion = "1.1.1.RELEASE"
-
-// compileOnly
 val springBootStarterWebVersion = "2.7.0"
 val springDocVersion = "1.6.6"
-val springOauthVersion = "1.6.6"
+val springOauthVersion = "5.7.1"
 val zalandoSpringProblemVersion = "0.27.0"
 val servletApiVersion = "4.0.1"
 val oktaSpringBootVersion = "2.1.5"
@@ -140,12 +138,6 @@ dependencies {
   // Use the Kotlin JDK 8 standard library.
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-  // Use the Kotlin test library.
-  testImplementation("org.jetbrains.kotlin:kotlin-test")
-
-  // Use the Kotlin JUnit integration.
-  testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
-
   implementation("org.hashids:hashids:${hashidsVersion}")
 
   implementation("io.swagger.parser.v3:swagger-parser-v3:${swaggerParserVersion}")
@@ -154,23 +146,29 @@ dependencies {
       "org.springframework.security.oauth.boot:spring-security-oauth2-autoconfigure:${springOauthAutoConfigureVersion}")
   implementation("org.springframework.security:spring-security-jwt:${springSecurityJwtVersion}")
 
-  compileOnly("org.springframework.boot:spring-boot-starter-web:${springBootStarterWebVersion}") {
+  implementation("org.springframework.boot:spring-boot-starter-web:${springBootStarterWebVersion}") {
     exclude(group = "org.springframework.boot", module = "spring-boot-starter-tomcat")
   }
 
-  compileOnly("org.springdoc:springdoc-openapi-ui:${springDocVersion}")
-  compileOnly("org.springdoc:springdoc-openapi-kotlin:${springDocVersion}")
-  compileOnly("org.zalando:problem-spring-web-starter:${zalandoSpringProblemVersion}")
-  compileOnly("javax.servlet:javax.servlet-api:${servletApiVersion}")
-  compileOnly("org.springframework.boot:spring-boot-starter-security")
-  compileOnly("org.springframework.security:spring-security-oauth2-jose:${springOauthVersion}")
-  compileOnly(
+  implementation("org.springdoc:springdoc-openapi-ui:${springDocVersion}")
+  implementation("org.springdoc:springdoc-openapi-kotlin:${springDocVersion}")
+  implementation("org.zalando:problem-spring-web-starter:${zalandoSpringProblemVersion}")
+  implementation("javax.servlet:javax.servlet-api:${servletApiVersion}")
+  implementation("org.springframework.boot:spring-boot-starter-security")
+  implementation("org.springframework.security:spring-security-oauth2-jose:${springOauthVersion}")
+  implementation(
       "org.springframework.security:spring-security-oauth2-resource-server:${springOauthVersion}")
-  compileOnly("com.okta.spring:okta-spring-boot-starter:${oktaSpringBootVersion}")
+  implementation("com.okta.spring:okta-spring-boot-starter:${oktaSpringBootVersion}")
 
   testImplementation(kotlin("test"))
   testImplementation(platform("org.junit:junit-bom:${jUnitBomVersion}"))
   testImplementation("org.junit.jupiter:junit-jupiter")
   testImplementation("io.mockk:mockk:${mockkVersion}")
   testImplementation("org.awaitility:awaitility-kotlin:${awaitilityKVersion}")
+
+  // Use the Kotlin test library.
+  testImplementation("org.jetbrains.kotlin:kotlin-test")
+
+  // Use the Kotlin JUnit integration.
+  testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
