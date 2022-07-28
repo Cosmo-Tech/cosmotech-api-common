9f608fb5654ed43ed9808d755a89479a235db149
LC-Cosmo <leopold.cramer@cosmotech.com>
2022-07-28 11:52:50 +0200

    add: redisTemplate

--- src/main/kotlin/com/cosmotech/api/config/RedisConfig.kt
+++ src/main/kotlin/com/cosmotech/api/config/RedisConfig.kt
@@ -5,7 +5,9 @@ package com.cosmotech.api.config
 import com.redis.om.spring.annotations.EnableRedisDocumentRepositories
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
 
 
 @Configuration
@@ -14,5 +16,12 @@ open class RedisConfig {
     @Bean
     open fun jedisConnectionFactory(): JedisConnectionFactory? {
         return JedisConnectionFactory()
+    }
+
+    @Bean
+    fun redisTemplate(redisConnectionFactory: RedisConnectionFactory?): RedisTemplate<*, *> {
+        val template = RedisTemplate<ByteArray, ByteArray>()
+        template.connectionFactory = redisConnectionFactory!!
+        return template
     }
 }
