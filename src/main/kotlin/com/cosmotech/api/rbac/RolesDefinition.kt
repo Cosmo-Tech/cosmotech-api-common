5bc28dfed0ddaffd324c28be9a1f41babf884cc4
Vincent Carluer <vcarluer@gmail.com>
2022-10-05 10:32:25 +0200

    NONE role

--- src/main/kotlin/com/cosmotech/api/rbac/RolesDefinition.kt
+++ src/main/kotlin/com/cosmotech/api/rbac/RolesDefinition.kt
@@ -10,6 +10,7 @@ const val ROLE_EDITOR = "editor"
 const val ROLE_ADMIN = "admin"
 const val ROLE_VALIDATOR = "validator"
 const val ROLE_USER = "user"
+const val ROLE_NONE = "none"
 
 // apply same format rules for permission for consistency
 const val PERMISSION_READ_DATA = "read_data"
@@ -20,6 +21,7 @@ const val PERMISSION_EDIT_SECURITY = "edit_security"
 const val PERMISSION_LAUNCH = "launch"
 const val PERMISSION_VALIDATE = "validate"
 
+val COMMON_ROLE_NONE_PERMISSIONS: List<String> = listOf()
 val COMMON_ROLE_READER_PERMISSIONS = listOf(PERMISSION_READ_DATA, PERMISSION_READ_SECURITY)
 val COMMON_ROLE_USER_PERMISSIONS =
     listOf(PERMISSION_READ_DATA, PERMISSION_READ_SECURITY, PERMISSION_CREATE_CHILDREN)
@@ -87,6 +89,7 @@ fun getCommonRolesDefinition(): RolesDefinition {
   return RolesDefinition(
       permissions =
           mutableMapOf(
+              ROLE_NONE to COMMON_ROLE_NONE_PERMISSIONS,
               ROLE_VIEWER to COMMON_ROLE_READER_PERMISSIONS,
               ROLE_USER to COMMON_ROLE_USER_PERMISSIONS,
               ROLE_EDITOR to COMMON_ROLE_EDITOR_PERMISSIONS,
@@ -99,6 +102,7 @@ fun getScenarioRolesDefinition(): RolesDefinition {
   return RolesDefinition(
       permissions =
           mutableMapOf(
+              ROLE_NONE to COMMON_ROLE_NONE_PERMISSIONS,
               ROLE_VIEWER to SCENARIO_ROLE_VIEWER_PERMISSIONS,
               ROLE_EDITOR to SCENARIO_ROLE_EDITOR_PERMISSIONS,
               ROLE_VALIDATOR to SCENARIO_ROLE_VALIDATOR_PERMISSIONS,
