c41bd2f821f9d9f44001104015088b01b9a85c98
icreated <sergey.polyarus.ext@cosmotech.com>
2023-01-16 14:20:25 +0100

    Sanitize Redis tests

---
+++ src/test/kotlin/com/cosmotech/api/utils/StringExtensionsTests.kt
@@ -0,0 +1,27 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+package com.cosmotech.api.utils
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class StringExtensionsTests {
+
+  @Test
+  fun `sanitizeForKubernetes should replace special characters with '-' and takeLast`() {
+    val input = "ALL.my/namespace:with-SPECIAL_chars"
+    val expected = "all-my-namespace-with-special-chars"
+    var actual = input.sanitizeForKubernetes()
+    assertEquals(expected, actual)
+    actual = input.sanitizeForKubernetes(13)
+    assertEquals("special-chars", actual)
+  }
+
+  @Test
+  fun `sanitizeForRedis should escape special characters`() {
+    val input = "test@cosmotech.com"
+    val expected = "test\\@cosmotech\\.com"
+    val actual = input.sanitizeForRedis()
+    assertEquals(expected, actual)
+  }
+}
