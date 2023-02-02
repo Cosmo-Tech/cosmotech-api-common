2ac4416f7474fd91f86675396ab652d764b721aa
Vincent Carluer <vcarluer@gmail.com>
2023-02-02 13:51:14 +0100

    New metrics module with new common PersistentMetric for scenario run first metrics

--- src/main/kotlin/com/cosmotech/api/metrics/PersistentMetric.kt
+++ src/main/kotlin/com/cosmotech/api/metrics/PersistentMetric.kt
@@ -4,13 +4,15 @@ package com.cosmotech.api.metrics
 
 private const val DEFAULT_PROVIDER = "cosmotech"
 
+private const val DEFAULT_QUALIFIER = "data"
+
 data class PersistentMetric(
     val service: String,
     val name: String,
     val value: Double,
     val incrementBy: Int = 0,
     val tags: Map<String, String> = emptyMap(),
-    val qualifier: String = "",
+    val qualifier: String = DEFAULT_QUALIFIER,
     val timestamp: Long = System.currentTimeMillis(),
     val vendor: String = DEFAULT_PROVIDER,
     val retention: Long = 0,
