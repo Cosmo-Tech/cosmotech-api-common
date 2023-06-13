147cec8c1a0ed3e34169705ea6056f2d4a076539
icreated <sergey.polyarus.ext@cosmotech.com>
2023-06-13 16:28:14 +0200

    Fix zip byteArray

--- src/main/kotlin/com/cosmotech/api/utils/ZipUtils.kt
+++ src/main/kotlin/com/cosmotech/api/utils/ZipUtils.kt
@@ -2,49 +2,35 @@
 // Licensed under the MIT license.
 package com.cosmotech.api.utils
 
-import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
-import java.io.IOException
 import java.io.InputStream
 import java.util.zip.ZipEntry
 import java.util.zip.ZipInputStream
 import java.util.zip.ZipOutputStream
 
-const val BUFFER_SIZE = 2048
-
-/** Data class representing a file name and its content as an InputStream for a zip file */
-data class UnzippedFile(val filename: String, val content: InputStream)
-
 /**
  * Unzip a zip file and return a list of files with their content
  * @param file The zip file to unzip
- * @param prefixName The prefix name of the files to unzip
+ * @param prefixNames The prefix names of the files or folders to unzip
  * @param fileExtension The file extension of the files to unzip
  */
-fun unzip(file: InputStream, prefixName: String, fileExtension: String): List<UnzippedFile> =
+fun unzip(file: InputStream, prefixNames: List<String>, fileExtension: String): List<UnzippedFile> =
     ZipInputStream(file).use { zipInputStream ->
       generateSequence { zipInputStream.nextEntry }
           .filterNot { it.isDirectory }
-          .filter { it.name.startsWith(prefixName, true) }
+          .filter { prefixNames.any { prefix -> it.name.startsWith(prefix, true) } }
           .filter { it.name.endsWith(fileExtension, true) }
           .map {
             UnzippedFile(
                 filename = it.name.extractFileNameFromPath(),
-                content = zipInputStream.toInputStream())
+                prefix = prefixNames.first { prefix -> it.name.startsWith(prefix, true) },
+                content = zipInputStream.readAllBytes())
           }
           .toList()
     }
 
-/** Get InputStream from ZipInputStream */
-@Throws(IOException::class)
-fun ZipInputStream.toInputStream(): InputStream {
-  val data = ByteArray(BUFFER_SIZE)
-  val out = ByteArrayOutputStream()
-  while (this.read(data, 0, BUFFER_SIZE) != -1) {
-    out.write(data)
-  }
-  return ByteArrayInputStream(out.toByteArray())
-}
+/** Data class representing a file name and its content */
+data class UnzippedFile(val filename: String, val prefix: String, val content: ByteArray)
 
 /**
  * Zip a map of file names and byte arrays into a single byte array
