4a95f99f6ab3c85a78cc45f2123ad1333556953b
Michel Guillon <michel.guillon@cosmotech.com>
2023-08-08 17:24:31 +0200

    Set dockerClient as a bean and add some tests

--- src/main/kotlin/com/cosmotech/api/containerregistry/DockerContainerRegistryClient.kt
+++ src/main/kotlin/com/cosmotech/api/containerregistry/DockerContainerRegistryClient.kt
@@ -4,10 +4,6 @@ package com.cosmotech.api.containerregistry
 
 import com.cosmotech.api.config.CsmPlatformProperties
 import com.github.dockerjava.api.DockerClient
-import com.github.dockerjava.core.DefaultDockerClientConfig
-import com.github.dockerjava.core.DockerClientImpl
-import com.github.dockerjava.httpclient5.ApacheDockerHttpClient
-import com.github.dockerjava.transport.DockerHttpClient
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Service
 
@@ -16,29 +12,16 @@ import org.springframework.stereotype.Service
     name = ["csm.platform.containerRegistry.provider"],
     havingValue = "local",
     matchIfMissing = true)
-class DockerContainerRegistryClient(private val csmPlatformProperties: CsmPlatformProperties) :
-    RegistryClient {
+open class DockerContainerRegistryClient(
+    private val csmPlatformProperties: CsmPlatformProperties,
+    private val dockerClient: DockerClient
+) : RegistryClient {
 
   override fun getEndpoint() = csmPlatformProperties.containerRegistry.registryUrl
 
   override fun checkSolutionImage(repository: String, tag: String) {
-    getDockerClient().inspectImageCmd(buildImageFullImageName(repository, tag)).exec()
-  }
-
-  private fun buildImageFullImageName(repository: String, tag: String) =
-      csmPlatformProperties.containerRegistry.registryUrl + "/" + repository + ":" + tag
-
-  private fun getDockerClient(): DockerClient {
-    val config =
-        DefaultDockerClientConfig.createDefaultConfigBuilder()
-            .withRegistryUrl(csmPlatformProperties.containerRegistry.registryUrl)
-            .withRegistryPassword(csmPlatformProperties.containerRegistry.registryPassword)
-            .withRegistryUsername(csmPlatformProperties.containerRegistry.registryUserName)
-            .build()
-
-    val httpClient: DockerHttpClient =
-        ApacheDockerHttpClient.Builder().dockerHost(config.getDockerHost()).build()
-
-    return DockerClientImpl.getInstance(config, httpClient)
+    val imageFullName =
+        csmPlatformProperties.containerRegistry.registryUrl + "/" + repository + ":" + tag
+    dockerClient.inspectImageCmd(imageFullName).exec()
   }
 }
