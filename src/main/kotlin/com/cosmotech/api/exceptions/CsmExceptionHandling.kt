110a70f47454b5737c6a260c72860469ec2abe36
Jérémy Reynard <jeremy.reynard@cosmotech.com>
2023-10-27 16:58:22 +0200

    CsmExceptionHandling rework using ProblemDetail

--- src/main/kotlin/com/cosmotech/api/exceptions/CsmExceptionHandling.kt
+++ src/main/kotlin/com/cosmotech/api/exceptions/CsmExceptionHandling.kt
@@ -2,23 +2,34 @@
 // Licensed under the MIT license.
 package com.cosmotech.api.exceptions
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ProblemDetail
+import org.springframework.security.authentication.InsufficientAuthenticationException
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 @RestControllerAdvice
 open class CsmExceptionHandling : ResponseEntityExceptionHandler() {
 
-/*
   @ExceptionHandler
   fun handleIllegalArgumentException(
       exception: IllegalArgumentException
   ): ProblemDetail {
-    return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, exception.message)
+    if (exception.message == null) {
+      return ProblemDetail.forStatus(HttpStatus.BAD_REQUEST)
+    }
+    return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST,exception.message!!)
   }
 
-  @ExceptionHandler
-  fun handleInsufficientAuthenticationException(
-      exception: InsufficientAuthenticationException,
-      request: NativeWebRequest
-  ): ResponseEntity<Problem> = create(Status.UNAUTHORIZED, exception, request)*/
+    @ExceptionHandler
+    fun handleInsufficientAuthenticationException(
+        exception: InsufficientAuthenticationException
+    ): ProblemDetail{
+      if (exception.message == null) {
+        return ProblemDetail.forStatus(HttpStatus.UNAUTHORIZED)
+      }
+      return ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED,exception.message!!)
+    }
+
 }
