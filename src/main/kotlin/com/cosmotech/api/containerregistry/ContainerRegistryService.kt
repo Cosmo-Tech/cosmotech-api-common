db6dccaa78291d9ef10e123956f9e73b7384a627
Michel Guillon <michel.guillon@cosmotech.com>
2023-11-07 14:56:41 +0100

    Use response status code and get tags data instead of checking a response as a string

--- src/main/kotlin/com/cosmotech/api/containerregistry/ContainerRegistryService.kt
+++ src/main/kotlin/com/cosmotech/api/containerregistry/ContainerRegistryService.kt
@@ -3,12 +3,16 @@
 package com.cosmotech.api.containerregistry
 
 import com.cosmotech.api.config.CsmPlatformProperties
+import com.cosmotech.api.exceptions.CsmClientException
 import com.cosmotech.api.exceptions.CsmResourceNotFoundException
 import java.util.Base64
+import org.apache.http.HttpStatus
 import org.apache.http.client.methods.HttpUriRequest
 import org.apache.http.client.methods.RequestBuilder
 import org.apache.http.impl.client.HttpClients
 import org.apache.http.util.EntityUtils
+import org.json.JSONArray
+import org.json.JSONObject
 import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Service
 
@@ -34,13 +38,16 @@ class ContainerRegistryService(private val csmPlatformProperties: CsmPlatformPro
   }
 
   fun checkSolutionImage(repository: String, tag: String) {
-    val tags = getRepositoryTagList(repository)
-    if (tags.contains("errors") || !doesTheTagExist(tags, tag)) {
-      throw CsmResourceNotFoundException(tags)
+    val images = getRepositoryTagList(repository)
+    if (!doesTheTagExist(images, tag)) {
+      throw CsmResourceNotFoundException(repository + ":" + tag)
     }
   }
 
-  fun doesTheTagExist(tags: String, tag: String) = tags.contains(tag)
+  fun doesTheTagExist(images: String, tag: String): Boolean {
+    val tags: JSONArray = JSONObject(images).get("tags") as JSONArray
+    return tags.contains(tag)
+  }
 
   private fun getImageRegistryUri(repository: String): String {
     return csmPlatformProperties.containerRegistry.registryUrl + "/v2/" + repository + "/tags/list"
@@ -48,6 +55,10 @@ class ContainerRegistryService(private val csmPlatformProperties: CsmPlatformPro
 
   private fun execRequest(repository: String): String {
     val httpResponse = getHttpClient().execute(buildHttpRequest(repository))
+    if (HttpStatus.SC_OK != httpResponse.statusLine.statusCode) {
+      throw throw CsmClientException(
+          "The repository ${repository} : " + "${httpResponse.statusLine.reasonPhrase}")
+    }
     return EntityUtils.toString(httpResponse.getEntity())
   }
   fun getRepositoryTagList(repository: String) = execRequest(repository)
