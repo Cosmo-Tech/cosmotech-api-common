2fef5c4998ce06ddd1901c65b990b0f12c80bb86
Laurent Alépée <laurent.alepee@cosmotech.com>
2024-01-30 11:04:38 +0100

    add start and stop run event

---
+++ src/main/kotlin/com/cosmotech/api/events/RunEvents.kt
@@ -0,0 +1,15 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+package com.cosmotech.api.events
+
+@Suppress("UnusedPrivateMember")
+class RunStart(
+  publisher: Any,
+  runner: Any
+) : CsmRequestResponseEvent<String>(publisher)
+
+@Suppress("UnusedPrivateMember")
+class RunStop (
+  publisher: Any,
+  run: Any
+) : CsmEvent(publisher)
