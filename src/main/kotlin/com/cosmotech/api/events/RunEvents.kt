7b75059c9c7a3702873e9a75b5533084bd821667
Laurent Alépée <laurent.alepee@cosmotech.com>
2024-02-16 17:44:22 +0100

    change startEvent response to any

--- src/main/kotlin/com/cosmotech/api/events/RunEvents.kt
+++ src/main/kotlin/com/cosmotech/api/events/RunEvents.kt
@@ -3,6 +3,6 @@
 package com.cosmotech.api.events
 
 @Suppress("UnusedPrivateMember")
-class RunStart(publisher: Any, runner: Any) : CsmRequestResponseEvent<String>(publisher)
+class RunStart(publisher: Any, runner: Any) : CsmRequestResponseEvent<Any>(publisher)
 
-@Suppress("UnusedPrivateMember") class RunStop(publisher: Any, run: Any) : CsmEvent(publisher)
+@Suppress("UnusedPrivateMember") class RunStop(publisher: Any, run: String) : CsmEvent(publisher)
