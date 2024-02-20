1371d76deb7de20bfea37a2ec6362d53591eade8
Laurent Alépée <laurent.alepee@cosmotech.com>
2024-02-20 16:41:53 +0100

    remove unecessary suppress warning

--- src/main/kotlin/com/cosmotech/api/events/RunEvents.kt
+++ src/main/kotlin/com/cosmotech/api/events/RunEvents.kt
@@ -2,7 +2,6 @@
 // Licensed under the MIT license.
 package com.cosmotech.api.events
 
-@Suppress("UnusedPrivateMember")
 class RunStart(publisher: Any, val runnerData: Any) : CsmRequestResponseEvent<String>(publisher)
 
-@Suppress("UnusedPrivateMember") class RunStop(publisher: Any, val runnerData: Any) : CsmEvent(publisher)
+class RunStop(publisher: Any, val runnerData: Any) : CsmEvent(publisher)
