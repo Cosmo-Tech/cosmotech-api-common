d15cee59ff59800daaf19a1e3be28a95d0e820d1
Jérémy Reynard <jeremy.reynard@cosmotech.com>
2024-02-21 15:28:28 +0100

    Rewrite LokiService and ContainerRegistryService
    - introduce RestClient from Spring Boot 3.2
    - add type in ProblemDetail in ControllerAdvice

--- src/main/kotlin/com/cosmotech/api/containerregistry/ContainerRegistryService.kt
+++ src/main/kotlin/com/cosmotech/api/containerregistry/ContainerRegistryService.kt
@@ -6,57 +6,43 @@ import com.cosmotech.api.config.CsmPlatformProperties
 import com.cosmotech.api.exceptions.CsmClientException
 import com.cosmotech.api.exceptions.CsmResourceNotFoundException
 import java.util.Base64
-import org.apache.hc.client5.http.fluent.Request
-import org.apache.hc.core5.http.HttpStatus.SC_OK
 import org.json.JSONArray
 import org.json.JSONObject
 import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Service
+import org.springframework.web.client.RestClient
+import org.springframework.web.client.RestClientException
 
 @Service("csmContainerRegistry")
 class ContainerRegistryService(private val csmPlatformProperties: CsmPlatformProperties) {
-  fun getEndpoint() = csmPlatformProperties.containerRegistry.registryUrl
 
-  private fun getCredentials(user: String, password: String): String =
-      Base64.getEncoder().encodeToString("$user:$password".toByteArray())
+  private val restClient =
+      RestClient.builder().baseUrl(csmPlatformProperties.containerRegistry.registryUrl).build()
 
-  private fun getHeaderAuthorization() =
-      "Basic " +
-          getCredentials(
-              csmPlatformProperties.containerRegistry.registryUserName!!,
-              csmPlatformProperties.containerRegistry.registryPassword!!)
-
-  private fun buildRequestClient(repository: String): Request {
-
-    return Request.get(getImageRegistryUri(repository))
-        .addHeader(HttpHeaders.AUTHORIZATION, getHeaderAuthorization())
+  private fun getHeaderAuthorization(): String {
+    val user = csmPlatformProperties.containerRegistry.registryUserName!!
+    val password = csmPlatformProperties.containerRegistry.registryPassword!!
+    val basicToken = Base64.getEncoder().encodeToString("$user:$password".toByteArray())
+    return "Basic $basicToken"
   }
 
   fun checkSolutionImage(repository: String, tag: String) {
-    val images = getRepositoryTagList(repository)
-    if (!doesTheTagExist(images, tag)) {
-      throw CsmResourceNotFoundException("$repository:$tag")
+    try {
+      val images =
+          restClient
+              .get()
+              .uri("/v2/$repository/tags/list")
+              .header(HttpHeaders.AUTHORIZATION, getHeaderAuthorization())
+              .retrieve()
+              .body(String::class.java)!!
+
+      val tags: JSONArray = JSONObject(images).get("tags") as JSONArray
+      if (!tags.contains(tag)) {
+        throw CsmResourceNotFoundException("$repository:$tag")
+      }
+    } catch (e: RestClientException) {
+      throw CsmClientException(
+          "The repository $repository:$tag check has thrown error : " + e.message, e)
     }
   }
-
-  private fun doesTheTagExist(images: String, tag: String): Boolean {
-    val tags: JSONArray = JSONObject(images).get("tags") as JSONArray
-    return tags.contains(tag)
-  }
-
-  private fun getImageRegistryUri(repository: String): String {
-    return csmPlatformProperties.containerRegistry.registryUrl + "/v2/" + repository + "/tags/list"
-  }
-
-  private fun execRequest(repository: String): String {
-    val response = buildRequestClient(repository).execute()
-    val httpResponse = response.returnResponse()
-    val content = response.returnContent()
-    if (SC_OK != httpResponse.code) {
-      throw throw CsmClientException("The repository $repository : " + httpResponse.reasonPhrase)
-    }
-    return content.toString()
-  }
-
-  fun getRepositoryTagList(repository: String) = execRequest(repository)
 }
