cb8f13be0b8e92c3b947dfba9c2681eb0a8d190b
Jérémy Reynard <jeremy.reynard@cosmotech.com>
2024-02-22 11:28:55 +0100

    Fix LokiService with new RestClient usage

--- src/main/kotlin/com/cosmotech/api/loki/LokiService.kt
+++ src/main/kotlin/com/cosmotech/api/loki/LokiService.kt
@@ -6,8 +6,10 @@ import com.cosmotech.api.config.CsmPlatformProperties
 import java.time.OffsetDateTime
 import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Service
+import org.springframework.util.LinkedMultiValueMap
 import org.springframework.web.client.RestClient
 import org.springframework.web.client.body
+import org.springframework.web.util.UriComponentsBuilder
 
 // Needed for authentication in multitenant mode
 // https://grafana.com/docs/loki/latest/operations/authentication/
@@ -29,14 +31,24 @@ class LokiService(private val csmPlatformProperties: CsmPlatformProperties) {
   private fun execRequest(namespace: String, podName: String): String {
     // TODO Change it regarding sjoubert remarks
     val startTime =
-        OffsetDateTime.now().minusDays(csmPlatformProperties.loki.queryDaysAgo).toString()
-    val endTime = OffsetDateTime.now().toString()
+        OffsetDateTime.now()
+            .minusDays(csmPlatformProperties.loki.queryDaysAgo)
+            .toInstant()
+            .toEpochMilli()
+            .toString()
+    val endTime = OffsetDateTime.now().toInstant().toEpochMilli().toString()
 
     val parameters = buildParameters(namespace, podName, startTime, endTime)
 
     return restClient
         .get()
-        .uri(csmPlatformProperties.loki.queryPath, parameters)
+        .uri { uriBuilder ->
+          UriComponentsBuilder.fromUri(
+                  uriBuilder.path(csmPlatformProperties.loki.queryPath).build())
+              .queryParams(parameters)
+              .build()
+              .toUri()
+        }
         .header(
             HttpHeaders.CONTENT_TYPE, "application/x-ndjson", "application/x-www-form-urlencoded")
         .header(CUSTOM_HEADER_TENANT_ID, csmPlatformProperties.namespace)
@@ -49,7 +61,13 @@ class LokiService(private val csmPlatformProperties: CsmPlatformProperties) {
       podName: String,
       startTime: String,
       endTime: String
-  ) = mapOf("query" to getQuery(namespace, podName), "start" to startTime, "end" to endTime)
+  ): LinkedMultiValueMap<String, String> {
+    val params = LinkedMultiValueMap<String, String>()
+    params.add("query", getQuery(namespace, podName))
+    params.add("start", startTime)
+    params.add("end", endTime)
+    return params
+  }
 
   private fun getQuery(namespace: String, podName: String) =
       "{namespace=\"$namespace\",pod=\"$podName\"}"
