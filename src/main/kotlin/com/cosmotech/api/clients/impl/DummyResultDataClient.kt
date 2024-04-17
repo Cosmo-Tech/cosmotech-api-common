056cc97bc5e4d4284ad3fa9ff1d852c2867b3c7b
Jérémy Reynard <jeremy.reynard@cosmotech.com>
2024-04-17 18:22:39 +0200

    Add EventBusClient and ResultDataClient interfaces
    - Add DummyEventBusClient
    - Add DummyResultDataClient

---
+++ src/main/kotlin/com/cosmotech/api/clients/impl/DummyResultDataClient.kt
@@ -0,0 +1,31 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+package com.cosmotech.api.clients.impl
+
+import com.cosmotech.api.clients.ResultDataClient
+import com.cosmotech.api.scenariorun.DataIngestionState
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
+import org.springframework.stereotype.Component
+
+@Component
+@Deprecated("Will be removed once cosmotech-api-scenario and cosmotech-api-scenario are removed")
+@ConditionalOnExpression("'\${csm.platform.use-internal-result-services}' == 'true'")
+class DummyResultDataClient : ResultDataClient {
+
+  override fun deleteDataFromADXbyExtentShard(
+      organizationId: String,
+      workspaceKey: String,
+      extentShard: String
+  ): String {
+    TODO("Not yet implemented")
+  }
+
+  override fun getStateFor(
+      organizationId: String,
+      workspaceKey: String,
+      scenarioRunId: String,
+      csmSimulationRun: String
+  ): DataIngestionState {
+    TODO("Not yet implemented")
+  }
+}
