dd7e8eaa8fb4697872be2676254fb0037fd5bc9e
Sylvain Joubert <sylvain.joubert@cosmotech.com>
2024-07-15 17:38:53 +0200

    [PROD-13629][PROD-13630] Fix some issues with the Loki query
    - Increase log count (was 100 by default)
    - Fix start/end time, these should be nanoseconds
    - Fix query range, use the job start time instead of querying only the
      last 24h (from the endpoint request...)
    
    This is the minimum effort fixes for this version, better logic will be
    implemented in the next one (no limit on line count, use dynamic loki
    config) along with a simpler return data structure for the log endpoint

--- src/main/kotlin/com/cosmotech/api/loki/LokiService.kt
+++ src/main/kotlin/com/cosmotech/api/loki/LokiService.kt
@@ -8,8 +8,19 @@ import org.apache.http.client.methods.HttpUriRequest
 import org.apache.http.client.methods.RequestBuilder
 import org.apache.http.impl.client.HttpClients
 import org.apache.http.util.EntityUtils
+import java.time.Duration
+import java.time.Instant
 import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Service
+
+private const val MILLI_TO_NANO = 1_000_000
+// Use the default value of loki for limits_config.max_query_length
+// This should be made dynamic based on the actual loki configuration in v4.0
+// Loki default is actually 30d1h
+private const val LOKI_MAX_QUERY_LENGTH_DAYS = 30L
+// Use the default value of loki for limits_config.max_entries_limit_per_query
+// This should be made dynamic based on the actual loki configuration in v4.0
+private const val LOKI_MAX_ENTRIES_LIMIT_PER_QUERY = "5000"
 
 // Needed for authentication in multitenant mode
 // https://grafana.com/docs/loki/latest/operations/authentication/
@@ -21,15 +32,15 @@ class LokiService(private val csmPlatformProperties: CsmPlatformProperties) {
     return csmPlatformProperties.loki.baseUrl + csmPlatformProperties.loki.queryPath
   }
 
-  private fun execRequest(namespace: String, podName: String): String {
-    val httpResponse = getHttpClient().execute(buildHttpRequest(namespace, podName))
+  private fun execRequest(namespace: String, podName: String, startTime: Instant): String {
+    val httpResponse = getHttpClient().execute(buildHttpRequest(namespace, podName,startTime))
     return EntityUtils.toString(httpResponse.getEntity())
   }
-  fun getPodLogs(namespace: String, podName: String) = execRequest(namespace, podName)
+  fun getPodLogs(namespace: String, podName: String, startTime: Instant) = execRequest(namespace, podName, startTime)
 
-  fun getPodsLogs(namespace: String, podNames: List<String>): Map<String, String> {
+  fun getPodsLogs(namespace: String, podNames: List<String>, startTime: Instant): Map<String, String> {
     var podsLogs = mutableMapOf<String, String>()
-    podNames.forEach { podsLogs[it] = getPodLogs(namespace, it)!! }
+    podNames.forEach { podsLogs[it] = getPodLogs(namespace, it, startTime)!! }
     return podsLogs
   }
 
@@ -38,18 +49,21 @@ class LokiService(private val csmPlatformProperties: CsmPlatformProperties) {
 
   private fun getHttpClient() = HttpClients.createDefault()
 
-  private fun buildHttpRequest(namespace: String, podName: String): HttpUriRequest {
+  private fun buildHttpRequest(namespace: String, podName: String, startTime: Instant): HttpUriRequest {
     var reqBuilder = RequestBuilder.get()
     reqBuilder = reqBuilder.setUri(getLokiQueryURI())
     reqBuilder = reqBuilder.addHeader(HttpHeaders.CONTENT_TYPE, "application/x-ndjson")
     reqBuilder = reqBuilder.addHeader(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded")
     reqBuilder = reqBuilder.addHeader(CUSTOM_HEADER_TENANT_ID, csmPlatformProperties.namespace)
     reqBuilder = reqBuilder.addParameter("query", getQuery(namespace, podName))
-    val startTime =
-        OffsetDateTime.now().minusDays(csmPlatformProperties.loki.queryDaysAgo).toString()
-    val endTime = OffsetDateTime.now().toString()
-    reqBuilder = reqBuilder.addParameter("start", startTime)
-    reqBuilder = reqBuilder.addParameter("end", endTime)
+    reqBuilder = reqBuilder.addParameter("limit", LOKI_MAX_ENTRIES_LIMIT_PER_QUERY)
+
+      val startTimeNano = startTime.toEpochMilli() * MILLI_TO_NANO
+      val endTimeNano =
+          startTime.plus(Duration.ofDays(LOKI_MAX_QUERY_LENGTH_DAYS)).toEpochMilli() * MILLI_TO_NANO
+
+    reqBuilder = reqBuilder.addParameter("start", startTimeNano)
+    reqBuilder = reqBuilder.addParameter("end", endTimeNano)
     return reqBuilder.build()
   }
 }
