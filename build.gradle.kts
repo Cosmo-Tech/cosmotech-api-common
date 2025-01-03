9656180279454b14037599c2144a3de798a73b96
Diamantis Sellis <diamantis.sellis@cosmotech.com>
2025-01-03 10:38:09 +0100

    chore: simplify build configuration
    The exact version of the kotlin formatter is not
    needed, we downgrade to a previous version for
    now and let the spotless plugin control the
    exact version. This simplifies the configuration
    and when in the future we upgrade the plugin
    version we will automatically upgrade the formatter
    version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -84,12 +84,10 @@ configure<SpotlessExtension> {
     licenseHeader(licenseHeaderComment)
   }
   kotlin {
-    ktfmt("0.50")
     target("**/*.kt")
     licenseHeader(licenseHeaderComment)
   }
   kotlinGradle {
-    ktfmt("0.50")
     target("**/*.kts")
     //      licenseHeader(licenseHeaderComment, "import")
   }
