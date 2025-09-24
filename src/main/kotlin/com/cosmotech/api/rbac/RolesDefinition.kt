ac11e4fbc642e3777798ddd43868a57496e7b3ff
Leopold Cramer <leopold.h.cramer@gmail.com>
2025-09-24 17:27:24 +0200

    none was not in roles definition and thus was unassignable

--- src/main/kotlin/com/cosmotech/api/rbac/RolesDefinition.kt
+++ src/main/kotlin/com/cosmotech/api/rbac/RolesDefinition.kt
@@ -22,6 +22,8 @@ const val PERMISSION_DELETE = "delete"
 const val PERMISSION_LAUNCH = "launch"
 const val PERMISSION_VALIDATE = "validate"
 
+val NO_PERMISSIONS = emptyList<String>()
+
 val COMMON_ROLE_READER_PERMISSIONS = listOf(PERMISSION_READ, PERMISSION_READ_SECURITY)
 val COMMON_ROLE_USER_PERMISSIONS =
     listOf(PERMISSION_READ, PERMISSION_READ_SECURITY, PERMISSION_CREATE_CHILDREN)
@@ -92,6 +94,7 @@ fun getCommonRolesDefinition(): RolesDefinition {
   return RolesDefinition(
       permissions =
           mutableMapOf(
+              ROLE_NONE to NO_PERMISSIONS,
               ROLE_VIEWER to COMMON_ROLE_READER_PERMISSIONS,
               ROLE_USER to COMMON_ROLE_USER_PERMISSIONS,
               ROLE_EDITOR to COMMON_ROLE_EDITOR_PERMISSIONS,
@@ -104,6 +107,7 @@ fun getScenarioRolesDefinition(): RolesDefinition {
   return RolesDefinition(
       permissions =
           mutableMapOf(
+              ROLE_NONE to NO_PERMISSIONS,
               ROLE_VIEWER to SCENARIO_ROLE_VIEWER_PERMISSIONS,
               ROLE_EDITOR to SCENARIO_ROLE_EDITOR_PERMISSIONS,
               ROLE_VALIDATOR to SCENARIO_ROLE_VALIDATOR_PERMISSIONS,
