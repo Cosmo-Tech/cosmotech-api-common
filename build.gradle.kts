136300eb478391dd40757094bb9f9cdab0c6a8ef
Jérémy Reynard <jeremy.reynard@cosmotech.com>
2025-10-13 10:08:43 +0200

    Update Testcontainers PostgreSQL and LocalStack versions to 1.21.3 in build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -180,8 +180,8 @@ val jUnitBomVersion = "5.12.2"
 val mockkVersion = "1.14.5"
 val awaitilityKVersion = "4.3.0"
 val testContainersRedisVersion = "1.6.4"
-val testContainersPostgreSQLVersion = "1.20.6"
-val testContainersLocalStackVersion = "1.20.6"
+val testContainersPostgreSQLVersion = "1.21.3"
+val testContainersLocalStackVersion = "1.21.3"
 
 dependencies {
   // https://youtrack.jetbrains.com/issue/KT-71057/POM-file-unusable-after-upgrading-to-2.0.20-from-2.0.10
